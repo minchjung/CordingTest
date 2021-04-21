764de6360d6dbf14e86d31a41f6ebb5c98ebd58b
minchjung <78589751+minchjung@users.noreply.github.com>
2021-04-21 19:25:43 +0900

    A. Petya and Strings [Implement]

---
+++ Codeforces Beta Round #85 (Div. 2)/A5.py
@@ -0,0 +1,11 @@
+# Codeforces Beta Round #85 (Div. 2 Only)
+# A. Petya and Strings
+#  Implement
+fst = input().lower()
+sec = input().lower()
+if fst > sec:
+    print(1)
+elif fst < sec : 
+    print(-1)
+else : 
+    print(0)
