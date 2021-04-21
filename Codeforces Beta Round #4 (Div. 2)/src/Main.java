52bd8ba43f9867d7688e49f2e0f50142935980b3
minchjung <78589751+minchjung@users.noreply.github.com>
2021-04-21 19:24:26 +0900

    A.WaterMelon [implement]

---
+++ Codeforces Beta Round #4 (Div. 2)/src/Main.java
@@ -0,0 +1,10 @@
+// Codeforces Beta Round #4 (Div. 2) A.WaterMelon
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in); 
+        int w = sc.nextInt(); 
+        if ( w % 2 == 0 && w > 2 ){System.out.println("YES"); }
+        else{System.out.println("NO"); }
+    }
+}
